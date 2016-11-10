6f1b5a7c30a5350a274919c8ae414fbe36242eab
Jared Rodgers <jrodger4@nd.edu>
2016-11-09 20:16:49 -0500

    Basic bluetooth connection code. Can turn on, off, see paired devices, and make discoverable.

--- app/BluetoothTest/app/src/main/java/com/patmyron/bluetoothtest/MainActivity.java
+++ app/BluetoothTest/app/src/main/java/com/patmyron/bluetoothtest/MainActivity.java
@@ -1,13 +1,75 @@
 package com.patmyron.bluetoothtest;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    Button bt_on,bt_off,bt_d;
+    private BluetoothAdapter BA;
+    private Set<BluetoothDevice> pairedDevices;
+    ListView lv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BA = BluetoothAdapter.getDefaultAdapter();
+
+        lv = (ListView) findViewById(R.id.listView);
+
+        bt_on = (Button) findViewById(R.id.button_bt_on);
+        bt_off = (Button)findViewById(R.id.button_bt_off);
+        bt_d = (Button)findViewById(R.id.button_bt_d);
+    }
+
+    // turns bluetooth on if not on
+    public void on(View v){
+        if (!BA.isEnabled()) {
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
+            Toast.makeText(getApplicationContext(), "Turned on",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Already on", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // turns bluetooth off
+    public void off(View v){
+        BA.disable();
+        Toast.makeText(getApplicationContext(), "Turned off" ,Toast.LENGTH_LONG).show();
+    }
+
+    //  makes device visible to bluetooth
+    public  void visible(View v){
+        Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        startActivityForResult(getVisible, 0);
+        BA.startDiscovery();
+    }
+
+    // lists paired devices
+    public void list(View v){
+        pairedDevices = BA.getBondedDevices();
+
+        ArrayList list = new ArrayList();
+
+        for(BluetoothDevice bt : pairedDevices) list.add(bt.getName());
+        Toast.makeText(getApplicationContext(), "Showing Paired Devices",Toast.LENGTH_SHORT).show();
+
+        final ArrayAdapter adapter = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+
+        lv.setAdapter(adapter);
     }
 }
