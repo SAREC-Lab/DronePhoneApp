620af8fd66b145b9776dc62bfdcb2cc32810724f
Pat Myron <pmyron09@gmail.com>
2016-11-30 23:39:00 -0500

    printing out gravity values on phone

--- app/BluetoothTest/app/src/main/java/com/patmyron/bluetoothtest/MainActivity.java
+++ app/BluetoothTest/app/src/main/java/com/patmyron/bluetoothtest/MainActivity.java
@@ -1,75 +1,41 @@
 package com.patmyron.bluetoothtest;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Set;
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-public class MainActivity extends AppCompatActivity {
-    Button bt_on,bt_off,bt_d;
-    private BluetoothAdapter BA;
-    private Set<BluetoothDevice> pairedDevices;
-    ListView lv;
-
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BA = BluetoothAdapter.getDefaultAdapter();
-
-        lv = (ListView) findViewById(R.id.listView);
-
-        bt_on = (Button) findViewById(R.id.button_bt_on);
-        bt_off = (Button)findViewById(R.id.button_bt_off);
-        bt_d = (Button)findViewById(R.id.button_bt_d);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
     }
 
-    // turns bluetooth on if not on
-    public void on(View v){
-        if (!BA.isEnabled()) {
-            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(turnOn, 0);
-            Toast.makeText(getApplicationContext(), "Turned on",Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(getApplicationContext(), "Already on", Toast.LENGTH_LONG).show();
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    // turns bluetooth off
-    public void off(View v){
-        BA.disable();
-        Toast.makeText(getApplicationContext(), "Turned off" ,Toast.LENGTH_LONG).show();
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
-    //  makes device visible to bluetooth
-    public  void visible(View v){
-        Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        startActivityForResult(getVisible, 0);
-        BA.startDiscovery();
-    }
-
-    // lists paired devices
-    public void list(View v){
-        pairedDevices = BA.getBondedDevices();
-
-        ArrayList list = new ArrayList();
-
-        for(BluetoothDevice bt : pairedDevices) list.add(bt.getName());
-        Toast.makeText(getApplicationContext(), "Showing Paired Devices",Toast.LENGTH_SHORT).show();
-
-        final ArrayAdapter adapter = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
-
-        lv.setAdapter(adapter);
+    public void onSensorChanged(SensorEvent event) {
+        ((TextView) findViewById(R.id.textView1)).setText(Float.toString(event.values[0]));
+        ((TextView) findViewById(R.id.textView2)).setText(Float.toString(event.values[1]));
+        ((TextView) findViewById(R.id.textView3)).setText(Float.toString(event.values[2]));
     }
 }
